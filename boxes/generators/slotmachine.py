da45f5059b6aa0b06717c1dc08f5c1f516260719
Scott Hassan <hassan@willowmail.com>
2017-10-10 11:57:38 -0700

    added the tray

--- boxes/generators/slotmachine.py
+++ boxes/generators/slotmachine.py
@@ -43,14 +43,14 @@ class SlotMachine(Boxes):
 
         # right
         self.corner(90)
-        edges.FingerJointSettings.surroundingspaces = 0.0
+        #edges.FingerJointSettings.surroundingspaces = 0.0
         self.edges["f"](self.front-self.flexheight/2)
         self.edges["X"](self.flexheight,self.width-t*2)
         self.edges["f"](self.fronttop-self.flexheight/2)
 
         # top
         self.corner(90)
-        edges.FingerJointSettings.surroundingspaces = 1.0
+        #edges.FingerJointSettings.surroundingspaces = 1.0
         self.edges["F"](self.width-t*2)
 
         # left
@@ -73,17 +73,17 @@ class SlotMachine(Boxes):
 
         ## tray
         self.ctx.restore()
-        tray_width = 100
-        tray_height = 50
+        cointray_width = 100
+        cointray_height = 50
 
         cx = self.width/2
         cy = self.front + self.fronttop/2
-        self.moveTo(cx - tray_width/2, 20)
-        self.rectangularWall(tray_width, tray_height, "eeee")
+        self.moveTo(cx - cointray_width/2, 20)
+        self.rectangularWall(cointray_width, cointray_height, "eeee")
 
         self.move(self.width+self.margin*2, self.front+self.margin, move)
 
-    def side(self, move=None):
+    def draw_side(self, move=None):
         if self.move(self.depth+self.margin*2, self.height+self.margin, move, True): return
 
         self.ctx.save()
@@ -95,7 +95,7 @@ class SlotMachine(Boxes):
 
         # right
         self.corner(90)
-        self.edges["F"](self.height - self.thickness)
+        self.edges["F"](self.height - self.thickness*2)
 
         # top
         self.corner(90)
@@ -103,30 +103,46 @@ class SlotMachine(Boxes):
 
         # left
         self.corner(60)
-        edges.FingerJointSettings.surroundingspaces = 0.0
+        #edges.FingerJointSettings.surroundingspaces = 0.0
         self.edges["F"](self.fronttop-self.flexheight/2)
         self.edges["e"](self.flexheight/2)
         self.corner(30)
         self.edges["e"](self.flexheight/2)
         self.edges["F"](self.front-self.flexheight/2)
-        edges.FingerJointSettings.surroundingspaces = 1.0
+        #edges.FingerJointSettings.surroundingspaces = 1.0
 
+        ## tray holes
         self.ctx.restore()
-        self.moveTo(self.thickness*2, self.height/2-20)
+        self.moveTo(self.thickness*2+30, self.height/2-20)
         self.continueDirection(math.radians(0))
-        self.edges["h"](self.depth - self.thickness*2)
+        self.edges["h"](self.tray_depth, no_continue=True)
 
         self.move(self.depth+self.margin*2, self.height+self.margin, move)
-
         return
 
+    def draw_tray(self, move=None):
+        if self.move(self.tray_width+self.margin, 0, move, True): return
+
+        self.moveTo(self.margin, 0)
+        self.edges["e"](self.tray_width)
+        self.corner(90)
+        self.edges["f"](self.tray_depth)
+        self.corner(90)
+        dx = (self.tray_width - self.hatch_width)/2
+        self.edges["f"](dx)
+        self.edges["e"](self.hatch_width)
+        self.edges["f"](dx)
+        self.corner(90)
+        self.edges["f"](self.tray_depth)
+        
+        self.move(self.tray_width+self.margin, 0, move)
+      
+
     def hatch_backing(self, move=None):
-        if self.move(self.hatch_width+20+self.margin, 0, move, True): return
+        if self.move(self.hatch_width+self.margin*2, 0, move, True): return
 
         cx = (self.hatch_width+20)/2
         cy = (self.hatch_height+20)/2
-
-        self.text("hatch backing", cx, cy, align="center")
 
         self.ctx.save()
         self.moveTo(cx - (self.hatch_width+20)/2, cy-(self.hatch_height+20)/2)
@@ -136,8 +152,33 @@ class SlotMachine(Boxes):
         self.moveTo(cx - (self.hatch_width-40)/2, cy-(self.hatch_height-40)/2)
         self.roundedPlate(self.hatch_width-40, self.hatch_height-40, 20, edge="e")
 
-        self.move(self.hatch_width+20+self.margin, 0, move)
+        self.move(self.hatch_width+self.margin*2, 0, move)
         
+    def draw_back(self, move=None):
+        if self.move(self.width+self.margin, 0, move, True): return
+      
+        self.ctx.save()
+        self.rectangularWall(self.width, self.height, "FfFf", move=None, exterior=True)
+        
+        ## draw hatch
+        cx = self.width/2
+        cy = self.height/2
+        self.moveTo(cx - self.hatch_width/2, cy-self.hatch_height/2)
+        self.roundedPlate(self.hatch_width, self.hatch_height, 20, edge="e")
+
+        self.ctx.restore()
+
+        ## draw tray holes
+        self.moveTo(self.thickness*1, self.height/2-20)
+        self.continueDirection(math.radians(0))
+        dx = cx - self.hatch_width/2
+
+        self.edges["h"](dx-self.thickness*1, no_continue=True)
+        self.moveTo(self.hatch_width+dx-self.thickness*1, 0)
+        self.edges["h"](dx, no_continue=True)
+        
+        self.move(self.width+self.margin, 0, move)
+
     def render(self):
         self.thickness = 3.1
         self.burn = .25
@@ -151,19 +192,23 @@ class SlotMachine(Boxes):
         self.margin = 10.0
         self.flexheight=20
 
-        self.topdepth = (self.depth-self.thickness*2) * .70
-        dx = (self.depth - self.topdepth)
+        indepth = self.depth-self.thickness*2
+        self.topdepth = indepth * .70
+        dx = (indepth - self.topdepth)
         self.fronttop = dx / math.cos(math.radians(60))
         self.front = (self.height-self.thickness*2) - (dx / math.tan(math.radians(30)))
 
         self.hatch_width = self.width * .80
         self.hatch_height = self.height * .80
 
+        self.tray_depth = self.depth - 30 - self.thickness*2
+        self.tray_width = self.width - self.thickness*2
+
         for key in ("width", "height", "depth", "topdepth", "flexheight",
                     "fronttop", "front", "burn", "thickness", "margin"):
           print ("%10s: %6.2f mm" % (key, getattr(self, key)))
 
-        edges.FingerJointSettings.surroundingspaces = 1.0
+        #edges.FingerJointSettings.surroundingspaces = 1.0
 
         # Initialize canvas
         self.open()
@@ -188,30 +233,25 @@ class SlotMachine(Boxes):
         self.moveTo(self.width, 0)
 
         self.text("left side", self.depth/2, self.height/2, align="center")
-        self.side(move="right")
+        self.draw_side(move="right")
 
         self.text("right side", self.depth/2, self.height/2, align="center")
-        self.side(move=None)
+        self.draw_side(move=None)
         cx = self.depth/2
         cy = self.height/2
         self.circle(cx, cy-self.height/4, 20)
         self.moveTo(self.depth+self.margin, 0)
 
-        # f's: bottom, right,top,left 
-        self.ctx.save()
+        # back
         self.text("back", self.width/2, self.height/2, align="center")
-        self.rectangularWall(self.width, self.height, "FfFf", move=None, exterior=True)
-        self.ctx.restore()
-        
-        ## draw hatch
-        cx = self.width/2
-        cy = self.height/2
-        self.moveTo(cx - self.hatch_width/2, cy-self.hatch_height/2)
-        self.roundedPlate(self.hatch_width, self.hatch_height, 20, edge="e")
-        self.moveTo(self.width, 0)
+        self.draw_back(move="right")
         
         ## draw hatch backing
+        self.text("hatch backing", self.hatch_width/2, self.hatch_height/2, align="center")
         self.hatch_backing("right")
+
+        self.text("tray", self.tray_width/2, self.tray_depth/2, align="center")
+        self.draw_tray("right")
 
         self.close()
 
