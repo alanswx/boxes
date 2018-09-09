acc3c0ae05ae49a18aacda7b2a051f92b18b9a44
Scott Hassan <hassan@willowmail.com>
2018-09-09 13:36:44 -0700

    new layout

--- boxes/generators/horsearcade.py
+++ boxes/generators/horsearcade.py
@@ -41,20 +41,23 @@ class HorseArcade(Boxes):
             with self.ctx:
                 self.label("leftSide", self.x/2, 25, align="center")
 
-                oldthickness = self.thickness
-                #self.thickness = 100.0
                 # glass holes
-                self.fingerHolesAt(self.thickness+self.tower_depth-self.thickness*2-self.candyDepth, self.candyHeight+self.corner_dy+self.h-self.thickness/2, self.tower_height-self.corner_dy-self.h, 90)
-                self.fingerHolesAt(self.thickness+self.tower_depth-self.thickness*2, self.candyHeight+self.corner_dy+self.h-self.thickness/2, self.tower_height-self.corner_dy-self.h, 90)
-                #self.thickness = oldthickness
-                self.fingerHolesAt(0, self.h-self.thickness/2, self.corner_dx+self.tower_depth+10, 0)
+                self.fingerHolesAt(self.thickness*2,
+                                   self.candyHeight+self.corner_dy+self.h-self.thickness/2, 
+                                   self.tower_height-self.corner_dy-self.h, 90)  #left
+                self.fingerHolesAt(self.thickness*2 + self.candyDepth,
+                                   self.candyHeight+self.corner_dy+self.h-self.thickness/2, 
+                                   self.tower_height-self.corner_dy-self.h, 90)  ## right
+
+                #self.fingerHolesAt(0, self.h-self.thickness/2, self.corner_dx+self.tower_depth, 0)
+                self.fingerHolesAt(0, self.h-self.thickness/2, self.y, 0)
                 self.fingerHolesAt(0, self.corner_dy+self.h-self.thickness/2, self.tower_depth, 0)
 
                 self.edges['e'](self.y)
                 self.corner(90)
-                self.edges['f'](self.h)
+                self.edges['f'](self.h + self.thickness + self.burn*2)
                 self.corner(90)
-                self.edges['F'](self.y - self.corner_dx - self.tower_depth)
+                self.edges['e'](self.y - self.corner_dx - self.tower_depth)
                 self.corner(-math.degrees(self.corner_angle_radians))
                 self.edges['e'](self.corner_length)
                 self.corner(-math.degrees(self.corner_angle_radians))
@@ -63,7 +66,7 @@ class HorseArcade(Boxes):
                 self.edges['e'](self.tower_depth)
                 self.corner(90)
                 self.edges['e'](self.tower_height-self.h)
-                self.edges['f'](self.h)
+                self.edges['f'](self.h + self.thickness + self.burn*2)
 
             #self.move(self.y+self.thickness, self.tower_height, move)
             self.move(self.corner_dx + self.tower_depth + self.thickness, self.h+self.thickness, move)
@@ -74,8 +77,12 @@ class HorseArcade(Boxes):
             with self.ctx:
                 self.label("rightStand", (self.corner_dx+self.tower_depth)/2, 25, align="center")
                 # glass holes
-                self.fingerHolesAt(self.thickness+self.tower_depth-self.thickness*2-self.candyDepth, self.candyHeight+self.corner_dy-self.thickness/2, self.tower_height-self.corner_dy-self.h, 90)
-                self.fingerHolesAt(self.thickness+self.tower_depth-self.thickness*2, self.candyHeight+self.corner_dy-self.thickness/2, self.tower_height-self.corner_dy-self.h, 90)
+                self.fingerHolesAt(self.thickness*2, 
+                                   self.candyHeight+self.corner_dy-self.thickness/2, 
+                                   self.tower_height-self.corner_dy-self.h, 90)
+                self.fingerHolesAt(self.thickness*2+self.candyDepth, 
+                                   self.candyHeight+self.corner_dy-self.thickness/2, 
+                                   self.tower_height-self.corner_dy-self.h, 90)
 
                 self.fingerHolesAt(0, self.corner_dy-self.thickness/2, self.tower_depth, 0)
 
@@ -94,8 +101,6 @@ class HorseArcade(Boxes):
 
     def pusherTray(self,move):
             self.ctx.save()
-
-
 
             with self.ctx:
                 self.edges['e'](self.candyWidth)
@@ -119,9 +124,14 @@ class HorseArcade(Boxes):
 
 
     def render(self):
-        self.show_labels = False
+        #self.show_labels = False
         self.show_labels = True
         self.thickness = 5.0
+        self.burn = .30
+        self.reference = 0.0
+
+        boxes.defaultStrokeColor = boxes.RED
+        boxes.holeStrokeColor = boxes.RED
 
         self.open()
         #x = 500
@@ -143,41 +153,69 @@ class HorseArcade(Boxes):
         self.corner_dx = self.h
         self.corner_length = math.sqrt(self.corner_dx*self.corner_dx+self.corner_dy*self.corner_dy)
         self.corner_angle_radians = math.atan(self.corner_dy/self.corner_dx)
-        self.tower_depth = 100
-        #self.tower_height = 600
-        self.tower_height = 900
+
         self.candyWidth=52
         self.candyDepth=91
-        self.candyHeight=12 # AJS MEASURE THIS
+        self.candyHeight=12
         self.bracketWidth=22
         self.bracketDepth=82
+
+        self.tower_depth = self.candyDepth + self.thickness * 4
+        self.tower_height = self.h + self.corner_dy + self.candyHeight * 50
 
         d2 = edges.Bolts(2)
         d3 = edges.Bolts(3)
 
         d2 = d3 = None
-        self.leftSide(move="right up")
-        self.rightStand(move="right")
+        with self.ctx:
+          self.leftSide(move=None)
+          
+        self.moveTo(self.corner_dx + self.tower_depth*2, 0)
 
-        #self.drawBoxOpening(25)
-        self.rectangularWall(x, h, "eFFF", bedBolts=[d2] * 4, move="up")
-        self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2], move="up")
-        #self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2], move="up")
-        #self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2],move="up")
-        self.rectangularWall(x, h, "eFFF", bedBolts=[d2] *4, move="up")
+        with self.ctx:
+          self.rightStand(move=None)
 
-        #self.rectangularHole(130/2+25,150/2+y/2,130,150)
-        self.hole(self.x-100-150, y/4, 24/2)
-        self.hole(self.x-100, y/4, 24/2)
-        self.fingerHolesAt(self.candyWidth,self.tower_depth, self.corner_dx+self.tower_depth, 90)
-        self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3], move="up")
+        self.moveTo(self.tower_depth *2, 0)
 
-        self.pusherTray(move="right up")
+        with self.ctx:
+          #self.drawBoxOpening(25)
+          self.rectangularWall(x, h, "eFFF", bedBolts=[d2] * 4, move=None)
+
+        self.moveTo(0, h + self.thickness*2)
+
+        with self.ctx:
+          self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2], move=None)
+
+          #self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2], move="up")
+          #self.rectangularWall(y, h, "efFf", bedBolts=[d3, d2, d3, d2],move="up")
+
+        self.moveTo(0, h + self.thickness*2)
+
+        with self.ctx:
+          self.rectangularWall(x, h, "eFFF", bedBolts=[d2] *4, move=None)
+
+        self.moveTo(0, h + self.thickness*2)
+
+        with self.ctx:
+          #self.rectangularHole(130/2+25,150/2+y/2,130,150)
+          self.hole(self.x-100-150, y/4, 24/2)
+          self.hole(self.x-100, y/4, 24/2)
+          self.fingerHolesAt(self.candyWidth,self.tower_depth, self.corner_dx+self.tower_depth, 90)
+
+          self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3], move=None)
+
+        self.moveTo(0, y + self.thickness*4)
+
+        with self.ctx:
+          self.pusherTray(move="right up")
 
         # bottom
+        
         #self.rectangularWall(x, y, "ffff", bedBolts=[d2, d3, d2, d3])
-        # plastic
-        self.rectangularWall(self.candyWidth, self.tower_height-self.corner_dy-self.h, "efef", bedBolts=[d2] *4, move="right")
-        self.rectangularWall(self.candyWidth, self.tower_height-self.corner_dy-self.h, "efef", bedBolts=[d2] *4, move="right")
+
+        if 0:
+          # plastic
+          self.rectangularWall(self.candyWidth, self.tower_height-self.corner_dy-self.h, "efef", bedBolts=[d2] *4, move="right")
+          self.rectangularWall(self.candyWidth, self.tower_height-self.corner_dy-self.h, "efef", bedBolts=[d2] *4, move="right")
 
         self.close()
