b5d5752ae6f51113f50c9e138b30609a136d0aac
Scott Hassan <hassan@willowmail.com>
2018-09-24 23:43:34 -0700

    deals with rotation

--- boxes/mergeSVG.py
+++ boxes/mergeSVG.py
@@ -6,9 +6,26 @@ import os, sys, string, time, logging, argparse
 import re
 import logging
 
+import svgutil
+
 _version = "0.1"
 
 import rectpack
+
+from lxml import etree as et
+from lxml import builder
+
+svg_ns = 'http://www.w3.org/2000/svg'
+
+class Part:
+  def __init__(self):
+    self.size = None
+    self.scale = None
+    self.viewbox = None
+    self.bbox = None
+    self.rotated = False
+
+    self.tree = None
 
 def parseSVGs(files):
   defpat = re.compile("<defs>(.+)</defs>", re.DOTALL)
@@ -22,48 +39,30 @@ def parseSVGs(files):
   for n, fn in enumerate(files):
     prefix = "%d:%s" % (n, fn)
     logging.debug("processing %s" % prefix)
-    body = open(fn, "r").read()
 
-    part = {}
+    parser = et.XMLParser(remove_blank_text=True)
+    src_tree = et.parse(fn, parser)
+
+    part = Part()
     parts.append(part)
-    part['rotated'] = False
 
-    m = viewboxpat.search(body)
-    if m:
-      part['vp_minx'] = int(m.group(1))
-      part['vp_miny'] = int(m.group(2))
-      part['vp_width'] = int(m.group(3))
-      part['vp_height'] = int(m.group(4))
+    part.size = svgutil.getSizeInMM(src_tree)
+    part.scale = svgutil.ticksPerMM(src_tree)
+    part.viewbox = svgutil.getViewBox(src_tree)
+    part.bbox = (part.viewbox[2], part.viewbox[3])
 
-      part['bbox'] = (part['vp_width'], part['vp_height'])
+    part.tree = src_tree
 
-    m = dimpat.search(body)
-    if m:
-      part['bbox2'] = (int(m.group(1)), int(m.group(2)))
-
-    m = defpat.search(body)
-    if m:
-      defs = m.group(1)
-      defs = defs.replace("glyph", "glyph%d_" % n)
-      part['defs'] = defs
-    else:
-      logging.debug("no def in %s" % fn)
-
-    m = drawpat.search(body)
-    if m:
-      draw = m.group(1)
-      part['draw'] = draw.replace("glyph", "glyph%d_" % n)
-
-    logging.debug(prefix + ": %s (%d %d %d %d)" % (part['bbox'], part['vp_minx'], part['vp_miny'], part['vp_width'], part['vp_height']))
+    logging.debug(prefix + ": size:%s viewbox:%s scale:%s" % (part.size, part.viewbox, part.scale))
 
   return parts
 
-def layoutParts(parts, viewport_size, margin=2, outermargin=10):
-  #packer = rectpack.newPacker(mode=rectpack.PackingMode.Offline, rotation=True)
-  packer = rectpack.newPacker(mode=rectpack.PackingMode.Offline, rotation=False)
+def layoutParts(parts, viewport_size, margin=4, outermargin=10):
+  packer = rectpack.newPacker(mode=rectpack.PackingMode.Offline, rotation=True)
+  #packer = rectpack.newPacker(mode=rectpack.PackingMode.Offline, rotation=False)
 
   for n,part in enumerate(parts):
-    rid = packer.add_rect(part['bbox'][0]+margin, part['bbox'][1]+margin, n)
+    rid = packer.add_rect(part.bbox[0]+margin, part.bbox[1]+margin, n)
 
   packer.add_bin(viewport_size[0]-outermargin*2, viewport_size[1]-outermargin*2, count=float("inf"))
 
@@ -78,19 +77,19 @@ def layoutParts(parts, viewport_size, margin=2, outermargin=10):
     for rect in abin:
       part = parts[rect.rid]
       page.append(part)
-      part['x'] = rect.x+margin//2
-      part['y'] = rect.y+margin//2
+      part.x = rect.x+margin//2
+      part.y = rect.y+margin//2
 
       rratio = rect.width / rect.height
-      pratio = part['bbox'][0] / part['bbox'][1]
+      pratio = part.bbox[0] / part.bbox[1]
 
       dr = abs(rratio - pratio)
 
       if dr > 0.1:
-        part['rotated'] = True
+        part.rotated = True
   return pages
 
-def writeSVG(outfn, parts, viewport_size, outermargin=10, units="mm"):
+def writeSVG(outfn, parts, viewport_size, margin=5, outermargin=10, units="mm"):
   minx = 12000
   miny = 12000
   vp_max_width = 0
@@ -99,14 +98,14 @@ def writeSVG(outfn, parts, viewport_size, outermargin=10, units="mm"):
   dimy = 0
 
   for n,part in enumerate(parts):
-    minx = min(minx, part['x'])
-    miny = min(miny, part['y'])
+    minx = min(minx, part.x)
+    miny = min(miny, part.y)
 
-    dimx += part['bbox'][0]
-    dimy = max(dimy, part['bbox'][1])
+    dimx += part.bbox[0]
+    dimy = max(dimy, part.bbox[1])
 
-    vp_max_width = max(vp_max_width, part['vp_width'])
-    vp_max_height = max(vp_max_height, part['vp_height'])
+    vp_max_width = max(vp_max_width, part.viewbox[2])
+    vp_max_height = max(vp_max_height, part.viewbox[3])
 
   #logging.debug("minx:%s miny:%s" % (minx, miny))
 
@@ -121,34 +120,73 @@ def writeSVG(outfn, parts, viewport_size, outermargin=10, units="mm"):
   fp.write('  sodipodi:docname="box.svg"\n')
 
   fp.write('  width="%d%s" height="%d%s"\n' % (viewport_size[0], units, viewport_size[1], units))
-  fp.write('  viewBox="%d %d %d %d"\n' % (minx-outermargin, miny+outermargin, viewport_size[0], viewport_size[1]))
+  viewBox = (minx-outermargin*2, miny+outermargin, viewport_size[0], viewport_size[1])
+  fp.write('  viewBox="%d %d %d %d"\n' % viewBox)
   fp.write('  version="1.1">\n')
 
   fp.write('<sodipodi:namedview id="namedview312" inkscape:document-units="%s" />\n' % units)
-
-  fp.write("<defs>\n")
-  for part in parts:
-    if 'defs' in part:
-      fp.write(part['defs'])
-  fp.write("</defs>\n")
-
-##  for r in range(0,90+15, 30):
-  if 1:
-    r = 90
-    for n,part in enumerate(parts):
-      if 'draw' in part:
-        fp.write('<g transform="')
-        if part['rotated']: fp.write('rotate(%d %d %d) ' % (r, 
-                                                            part['vp_minx'], 
-                                                            part['vp_miny']))
-        fp.write('translate(%d %d) ' % (part['x'], -part['y']))
-        fp.write('">\n')
-        fp.write(part['draw'])
-        fp.write('</g>\n')
-
   fp.write("</svg>\n")
-
   fp.close()
+  
+  parser = et.XMLParser(remove_blank_text=True)
+  body = open(outfn, "r")
+  dest_tree = et.parse(outfn, parser)
+  dest_root = dest_tree.getroot()
+
+  xlink = '{http://www.w3.org/1999/xlink}href'
+  crosses = []
+  for n, part in enumerate(parts):
+    for el in part.tree.getroot():
+      dest_root.append(el)
+
+      cx = viewBox[0]+part.x + margin + part.bbox[0]//2
+      cy = viewBox[1]+viewBox[3]-part.y - margin - part.bbox[1]//2
+      #crosses.append(cross((cx, cy), 10))
+
+      cx = viewBox[0]+part.x + margin
+      cy = viewBox[1]+viewBox[3]-part.y - margin 
+      crosses.append(cross((cx, cy), 10))
+
+      if el.tag.endswith("g"):
+        if part.rotated:
+          rotation = 90
+          el.set("transform", 'rotate(%d %d %d) translate(%d %d)' % (rotation, 
+                                                                     cx, cy,
+                                                                     part.x - part.bbox[0], -part.y))
+        else:
+          el.set("transform", 'translate(%d %d)' % (part.x, -part.y))
+      elif el.tag.endswith("defs"):
+        for child in el.iterfind(".//{%s}symbol" % svg_ns):
+          child.attrib['id'] = child.attrib['id'].replace("glyph", "glyph%d_" % n)
+      for child in el.iterfind(".//{%s}use" % svg_ns):
+        if xlink in child.attrib:
+          child.attrib[xlink] = child.attrib[xlink].replace("glyph", "glyph%d_" % n)
+
+  if 0:
+    E = builder.ElementMaker(namespace=svg_ns)
+    group = E.g(transform="translate(%d %d)" % (outermargin, outermargin))
+    dest_root.insert(0, group)
+
+  if 0:
+    for _cross in crosses:
+      dest_root.append(_cross)
+
+  if 0:
+    dest_root.append(cross((0,0)))
+    dest_root.append(cross((viewBox[0], viewBox[1])))
+    dest_root.append(cross((viewBox[0]+viewBox[2], viewBox[1])))
+    dest_root.append(cross((viewBox[0]+viewBox[2], viewBox[1]+viewBox[3])))
+    dest_root.append(cross((viewBox[0], viewBox[1]+viewBox[3])))
+
+  et.ElementTree(dest_root).write(outfn, pretty_print=True, encoding='utf-8', xml_declaration=True)
+
+def cross(pos, size=50):
+  E = builder.ElementMaker(namespace=svg_ns)
+  group = E.g()
+  group.append(E.line(x1=str(pos[0]-size), y1=str(pos[1]), x2=str(pos[0]+size), y2=str(pos[1]), style="stroke:rgb(255,0,0);stroke-width:2"))
+  group.append(E.line(x1=str(pos[0]), y1=str(pos[1]-size), x2=str(pos[0]), y2=str(pos[1]+size), style="stroke:rgb(255,0,0);stroke-width:2"))
+  return group
+  
 
 def mergeSVG(outfn, viewport_size, files, margin=2, outermargin=5, units="mm"):
   parts = parseSVGs(files)
@@ -165,7 +203,7 @@ def mergeSVG(outfn, viewport_size, files, margin=2, outermargin=5, units="mm"):
     except TypeError:
       fn = outfn
     logging.debug("writing SVG %s" % fn)
-    writeSVG(fn, _parts, viewport_size, outermargin, units)
+    writeSVG(fn, _parts, viewport_size, margin, outermargin, units)
 
 def start(args):
   outfn = "box.svg"
